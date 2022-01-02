3ccd6eabd1cb0307b6c5e95d347ab67893a49585
Alexey <e-post@lenta.ru>
2022-01-02 19:10:47 +0300

    условия если

---
+++ Razd2_lek_13 (uslovia).js
@@ -0,0 +1,24 @@
+'use strict';
+if (87==96) {
+    console.log ('Ok!');
+
+} 
+else {
+    
+    console.log('Error');
+
+}
+
+const number = 101;
+
+if (number < 100) 
+{console.log ('Число меньше 100') } 
+else if (number ==99)
+{console.log ('Число равно 99') }
+
+if (number > 99)
+{console.log ('Число больше 100') }
+
+;
+
+

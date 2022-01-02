f43f2ec69469362fce1c82791dd791baf40a747e
Alexey <e-post@lenta.ru>
2022-01-02 15:15:36 +0300

    запись ответов promt в объект movies

--- Razd2_lek_12.js
+++ Razd2_lek_12.js
@@ -1,3 +1,24 @@
 const numberOfFilms = [];
 numberOfFilms [0] = prompt("Сколько фильмов Вы посмотрели ?", "");
-document.write(numberOfFilms);
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat:false
+
+};
+
+
+const lastFilms1 = prompt("Один из последних фильмов которые Вы смотрели ?", ""),
+    allOfFilms2 = +prompt("Насколько баллов Вы его оцените ?", ""),
+    lastFilms3 = prompt("Один из последних фильмов которые Вы смотрели ?", ""),
+    allOfFilms4 = +prompt("Насколько баллов Вы его оцените ?", "");
+
+personalMovieDB.movies [lastFilms1]= allOfFilms2;
+personalMovieDB.movies [lastFilms3]= allOfFilms4;
+
+
+
+    console.log ( personalMovieDB);

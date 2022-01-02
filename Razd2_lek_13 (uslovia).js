a28a562dd12431aeb1967816265c6a8bd8e31f8e
Alexey <e-post@lenta.ru>
2022-01-02 22:32:40 +0300

    if, else, switch

--- Razd2_lek_13 (uslovia).js
+++ Razd2_lek_13 (uslovia).js
@@ -9,16 +9,30 @@ else {
 
 }
 
-const number = 101;
+const number = 50;
 
 if (number < 100) 
-{console.log ('Число меньше 100') } 
+{console.log ('Число меньше 100'); } 
 else if (number ==99)
-{console.log ('Число равно 99') }
+{console.log ('Число равно 99'); }
 
 if (number > 99)
-{console.log ('Число больше 100') }
+{console.log ('Число больше 100'); }
 
-;
+(number === 50) ? (console.log ("число равно number!")) : (console.log ("не равно number"));
 
+const number2=652;
+switch (number2) {
+    case 99:
+        console.log ('Не верно!');
+        break;
+    case 150:
+        console.log ('Не верно!');
+        break;
+    case 100:
+    console.log ('Вы угадали!!!');
+    break;
+    default:
+        console.log ('Вы ничего не угадали!!!!!!!');
 
+}
